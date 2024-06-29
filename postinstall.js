eb42cf0a55c2615c68b09756f2015c639083953d
rn0x <rn0x.me@gmail.com>
2024-06-29 09:39:47 +0300

    fix: update outputDir path in postinstall.js to 'src/yt-dlp'

--- postinstall.js
+++ postinstall.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 
 (async () => {
     try {
-        const outputDir = path.resolve(__dirname, 'yt-dlp');
+        const outputDir = path.resolve(__dirname, 'src', 'yt-dlp');
         const ytDlp = await setupYtDlp(outputDir, { log: true });
         console.log('yt-dlp downloaded and extracted:', ytDlp);
     } catch (error) {
